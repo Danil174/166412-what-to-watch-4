6a68b91a48639974d49f1a573a4b17c9dc08b3dd
Danil <milostiviy.danil@yandex.ru>
2020-06-13 12:57:43 +0500

    add mock filmsList

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {releaseDate, filmGenre} = props;
+  const {releaseDate, filmGenre, films} = props;
   return (
     <Main
       date={releaseDate}
       genre={filmGenre}
+      filmsList = {films}
     />
   );
 };

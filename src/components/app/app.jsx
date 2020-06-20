c97f7fb3c0e8eaa384da70bd073f74b9e80cf7c9
Danil <milostiviy.danil@yandex.ru>
2020-06-20 13:28:34 +0500

    app -> class component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 
 import Main from "../main/main.jsx";
 
-const App = (props) => {
-  const {releaseDate, filmGenre, films} = props;
-  return (
-    <Main
-      date={releaseDate}
-      genre={filmGenre}
-      filmsList = {films}
-    />
-  );
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    const {releaseDate, filmGenre, films} = this.props;
+
+    return (
+      <Main
+        date={releaseDate}
+        genre={filmGenre}
+        filmsList = {films}
+      />
+    );
+  }
+}
 
 App.propTypes = {
   releaseDate: PropTypes.number.isRequired,

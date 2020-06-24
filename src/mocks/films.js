4d2eb6416d0642a3538662bd2ddc4e47484756bf
Danil <milostiviy.danil@yandex.ru>
2020-06-24 14:02:31 +0500

    update mocks

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,77 +2,96 @@ export default [
   {
     title: `Aviator`,
     src: `img/aviator.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Rain_Fire.mov57604.webm`,
   },
   {
     title: `Bohemian Rhapsody`,
     src: `img/bohemian-rhapsody.jpg`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   },
   {
     title: `Dardjeeling Limited`,
     src: `img/dardjeeling-limited.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     title: `Fantastic Beasts The Crimes Of Grindelwald`,
     src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_12/preview/flashes2.mov43463.webm`,
   },
   {
     title: `Johnny English`,
     src: `img/johnny-english.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Macbeth`,
     src: `img/macbeth.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Midnight Special`,
     src: `img/midnight-special.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Mindhunter`,
     src: `img/mindhunter.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Moonrise Kingdom`,
     src: `img/moonrise-kingdom.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `No Country For Old Men`,
     src: `img/no-country-for-old-men.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Orlando`,
     src: `img/orlando.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Pulp Fiction`,
     src: `img/pulp-fiction.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Revenant`,
     src: `img/revenant.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Seven Years in Tibet`,
     src: `img/seven-years-in-tibet.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Shutter Island`,
     src: `img/shutter-island.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `Snatch`,
     src: `img/snatch.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `War of the Worlds`,
     src: `img/war-of-the-worlds.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `We Need to Talk about Kevin`,
     src: `img/we-need-to-talk-about-kevin.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   },
   {
     title: `What We Do in the Shadows`,
     src: `img/what-we-do-in-the-shadows.jpg`,
+    video: `https://www.videvo.net/videvo_files/converted/2014_08/preview/Fuego_de_Refineria.mp416312.webm`,
   }
 ];

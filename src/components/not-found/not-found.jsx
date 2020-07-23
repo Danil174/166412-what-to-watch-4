5db23c50f17eb1d20176c6f3c189b6d7afb051b3
Danil <milostiviy.danil@yandex.ru>
2020-07-23 15:10:41 +0500

    add not-found page

---
+++ src/components/not-found/not-found.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="/" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">
+          404.
+          <br />
+          <small>Page not found</small>
+        </h1>
+      </header>
+
+      <div className="user-page__content">
+        <Link className="page-title user-page__title" to="/">Go to main page</Link>
+      </div>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default NotFound;

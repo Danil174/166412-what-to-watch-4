95785c0c85ba55a8465bdf26ac0d8cfb27cd17ea
Danil <milostiviy.danil@yandex.ru>
2020-08-02 18:06:55 +0500

    fix bug with reviews when pick film from catalog--like-this

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {getActiveMovieTab} from "../../reducer/app-state/selectors.js";
-import {getFilms} from "../../reducer/data/selectors.js";
+import {getFilms, getComments} from "../../reducer/data/selectors.js";
 import {getAuthorizationStatus} from "../../reducer/user/selectors.js";
+import {Operation as DataOperation, ActionCreator} from '../../reducer/data/data.js';
 import {MovieTabs, MovieTabsMap, AppRoute, SIMILAR_LIST_LENGTH, AuthorizationStatus} from "../../const.js";
 
 import UserBlock from "../user-block/user-block.jsx";
@@ -15,158 +16,179 @@ import MovieDetails from "../movie-details/movie-details.jsx";
 import MovieReviews from "../movie-reviews/movie-reviews.jsx";
 import MoviesList from "../movies-list/movies-list.jsx";
 
-const getSelectedTab = (tab, film) => {
+const getSelectedTab = (tab, film, comments) => {
   switch (tab) {
     case MovieTabsMap.OVERVIEW:
       return <MovieOverview film={film}/>;
     case (MovieTabsMap.DETAILS):
       return <MovieDetails film={film} />;
     case (MovieTabsMap.REVIEWS):
-      return <MovieReviews filmID={film.id} />;
+      return <MovieReviews comments={comments} />;
     default:
       return <>error</>;
   }
 };
 
-const MoviePage = (props) => {
-  const {film, activeTab, films, authorizationStatus} = props;
-  const {
-    poster,
-    cover,
-    title,
-    genre,
-    releaseDate,
-    isFavorite,
-    bgColor,
-    id,
-  } = film;
+class MoviePage extends PureComponent {
+  componentDidMount() {
+    const {componentMounted, film} = this.props;
+    componentMounted(film.id);
+  }
 
-  const isAuth = authorizationStatus === AuthorizationStatus.AUTH;
+  componentDidUpdate(prevProps) {
+    if (prevProps.film.id !== this.props.film.id) {
+      const {componentMounted, film} = this.props;
+      componentMounted(film.id);
+    }
+  }
 
-  const similarFilms = films.filter((it) => it.genre === genre).slice(0, SIMILAR_LIST_LENGTH);
+  componentWillUnmount() {
+    this.props.componentUnmounted();
+  }
 
-  const posterAlt = `${title} poster`;
+  render() {
+    const {film, activeTab, films, authorizationStatus, comments} = this.props;
+    const {
+      poster,
+      cover,
+      title,
+      genre,
+      releaseDate,
+      isFavorite,
+      bgColor,
+      id,
+    } = film;
 
-  const sectionColor = {backgroundColor: bgColor};
+    const isAuth = authorizationStatus === AuthorizationStatus.AUTH;
 
-  return (
-  <>
-    <section
-      style={sectionColor}
-      className="movie-card movie-card--full"
-    >
-      <div className="movie-card__hero">
-        <div className="movie-card__bg">
-          <img
-            src={cover}
-            alt={title}
-          />
+    const similarFilms = films.filter((it) => it.genre === genre).slice(0, SIMILAR_LIST_LENGTH);
+
+    const posterAlt = `${title} poster`;
+
+    const sectionColor = {backgroundColor: bgColor};
+
+    return (
+    <>
+      <section
+        style={sectionColor}
+        className="movie-card movie-card--full"
+      >
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img
+              src={cover}
+              alt={title}
+            />
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <header className="page-header movie-card__head">
+            <div className="logo">
+              <a href="/" className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <div className="user-block">
+              <UserBlock />
+            </div>
+          </header>
+
+          <div className="movie-card__wrap">
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{title}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{releaseDate}</span>
+              </p>
+
+              <div className="movie-card__buttons">
+
+                <Link
+                  to={`${AppRoute.PLAYER_PAGE}/${id}`}
+                  className="btn btn--play movie-card__button"
+                  type="button"
+                >
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </Link>
+
+                <AddToList
+                  id={id}
+                  isFavorite={isFavorite}
+                />
+                {
+                  isAuth &&
+                  <Link
+                    to={`${AppRoute.MOVIE_PAGE}/${id}${AppRoute.REVIEW}`}
+                    className="btn movie-card__button"
+                  >
+                    Add review
+                  </Link>
+                }
+              </div>
+            </div>
+          </div>
         </div>
 
-        <h1 className="visually-hidden">WTW</h1>
+        <div className="movie-card__wrap movie-card__translate-top">
+          <div className="movie-card__info">
+            <div className="movie-card__poster movie-card__poster--big">
+              <img
+                src={poster}
+                alt={posterAlt}
+                width="218"
+                height="327"
+              />
+            </div>
 
-        <header className="page-header movie-card__head">
+            <div className="movie-card__desc">
+
+              <MovieNav tabs={MovieTabs} />
+
+              {getSelectedTab(activeTab, film, comments)}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+
+          <MoviesList
+            filmsList={similarFilms}
+          />
+        </section>
+
+        <footer className="page-footer">
           <div className="logo">
-            <a href="/" className="logo__link">
+            <a href="/" className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
             </a>
           </div>
 
-          <div className="user-block">
-            <UserBlock />
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-        </header>
-
-        <div className="movie-card__wrap">
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">{title}</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">{genre}</span>
-              <span className="movie-card__year">{releaseDate}</span>
-            </p>
-
-            <div className="movie-card__buttons">
-
-              <Link
-                to={`${AppRoute.PLAYER_PAGE}/${id}`}
-                className="btn btn--play movie-card__button"
-                type="button"
-              >
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </Link>
-
-              <AddToList
-                id={id}
-                isFavorite={isFavorite}
-              />
-              {
-                isAuth &&
-                <Link
-                  to={`${AppRoute.MOVIE_PAGE}/${id}${AppRoute.REVIEW}`}
-                  className="btn movie-card__button"
-                >
-                  Add review
-                </Link>
-              }
-            </div>
-          </div>
-        </div>
+        </footer>
       </div>
-
-      <div className="movie-card__wrap movie-card__translate-top">
-        <div className="movie-card__info">
-          <div className="movie-card__poster movie-card__poster--big">
-            <img
-              src={poster}
-              alt={posterAlt}
-              width="218"
-              height="327"
-            />
-          </div>
-
-          <div className="movie-card__desc">
-
-            <MovieNav tabs={MovieTabs} />
-
-            {getSelectedTab(activeTab, film)}
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <div className="page-content">
-      <section className="catalog catalog--like-this">
-        <h2 className="catalog__title">More like this</h2>
-
-        <MoviesList
-          filmsList={similarFilms}
-        />
-      </section>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="/" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
-  </>
-  );
-};
+    </>
+    );
+  }
+}
 
 MoviePage.propTypes = {
+  comments: PropTypes.array.isRequired,
+  componentMounted: PropTypes.func.isRequired,
+  componentUnmounted: PropTypes.func.isRequired,
   authorizationStatus: PropTypes.string.isRequired,
   activeTab: PropTypes.string.isRequired,
   films: PropTypes.array.isRequired,
@@ -184,10 +206,21 @@ MoviePage.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
+  comments: getComments(state),
   authorizationStatus: getAuthorizationStatus(state),
   activeTab: getActiveMovieTab(state),
   films: getFilms(state),
 });
 
+
+const mapDispatchToProps = (dispatch) => ({
+  componentMounted(id) {
+    dispatch(DataOperation.loadComments(id));
+  },
+  componentUnmounted() {
+    dispatch(ActionCreator.deleteComments());
+  }
+});
+
 export {MoviePage};
-export default connect(mapStateToProps)(MoviePage);
+export default connect(mapStateToProps, mapDispatchToProps)(MoviePage);
--- src/components/movie-reviews/movie-reviews.jsx
+++ src/components/movie-reviews/movie-reviews.jsx
@@ -1,156 +1,54 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {Operation as DataOperation, ActionCreator} from '../../reducer/data/data.js';
-import {getComments} from "../../reducer/data/selectors.js";
 import {Months} from "../../const.js";
 
-class MovieReviews extends PureComponent {
-  componentDidMount() {
-    const {filmID, componentMounted} = this.props;
-    componentMounted(filmID);
-  }
+const renderComment = (comment) => {
+  const {userID, userName, text, rating, date} = comment;
+  const prittydate = `${Months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+  return (
+    <div
+      key={`${userID}${date}`}
+      className="review">
+      <blockquote className="review__quote">
+        <p className="review__text">{text}</p>
 
-  componentWillUnmount() {
-    const {componentUnmounted} = this.props;
-    componentUnmounted();
-  }
+        <footer className="review__details">
+          <cite className="review__author">{userName}</cite>
+          <time className="review__date" dateTime="2016-12-24">{prittydate}</time>
+        </footer>
+      </blockquote>
 
-  _renderComment(comment) {
-    const {userName, text, rating, date} = comment;
-    const prittydate = `${Months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-    return (
-      <div className="review">
-        <blockquote className="review__quote">
-          <p className="review__text">{text}</p>
-
-          <footer className="review__details">
-            <cite className="review__author">{userName}</cite>
-            <time className="review__date" dateTime="2016-12-24">{prittydate}</time>
-          </footer>
-        </blockquote>
-
-        <div className="review__rating">{rating}</div>
-      </div>
-    );
-  }
-
-  // _createReviewsColumn(comments) {
-  //   return (
-  //     <>
-  //     </>
-  //   );
-  // }
-
-  render() {
-    const {comments} = this.props;
-    console.log(comments);
-    return (
-      <div className="movie-card__reviews movie-card__row">
-        <div className="movie-card__reviews-col">
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the directors funniest and most exquisitely designed movies in years.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Kate Muir</cite>
-                <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">8,9</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">Andersons films are too precious for some, but for those of us willing to lose ourselves in them, theyre a delight. The Grand Budapest Hotel is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Bill Goodykoontz</cite>
-                <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">8,0</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">I didnt find it amusing, and while I can appreciate the creativity, its an hour and 40 minutes I wish I could take back.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Amanda Greever</cite>
-                <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">8,0</div>
-          </div>
-        </div>
-        <div className="movie-card__reviews-col">
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Matthew Lickona</cite>
-                <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">7,2</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Paula Fleri-Soler</cite>
-                <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">7,6</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Paula Fleri-Soler</cite>
-                <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">7,0</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-MovieReviews.propTypes = {
-  componentMounted: PropTypes.func.isRequired,
-  componentUnmounted: PropTypes.func.isRequired,
-  filmID: PropTypes.number.isRequired,
+      <div className="review__rating">{rating}</div>
+    </div>
+  );
 };
 
+const MovieReviews = (props) => {
+  let {comments} = props;
 
-const mapStateToProps = (state) => ({
-  comments: getComments(state),
-});
+  const secondPart = comments.splice(0, comments.length / 2);
 
-const mapDispatchToProps = (dispatch) => ({
-  componentMounted(id) {
-    dispatch(DataOperation.loadComments(id));
-  },
-  componentUnmounted() {
-    dispatch(ActionCreator.deleteComments());
-  }
-});
+  return (
+    <div className="movie-card__reviews movie-card__row">
+      <div className="movie-card__reviews-col">
+        {comments.map((it) => renderComment(it))}
+      </div>
+      <div className="movie-card__reviews-col">
+        {secondPart.map((it) => renderComment(it))}
+      </div>
+    </div>
+  );
+};
 
-export {MovieReviews};
-export default connect(mapStateToProps, mapDispatchToProps)(MovieReviews);
+MovieReviews.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    commentID: PropTypes.number,
+    date: PropTypes.date,
+    rating: PropTypes.number,
+    text: PropTypes.string,
+    userID: PropTypes.number,
+    userName: PropTypes.string,
+  })).isRequired
+};
+
+export default MovieReviews;
